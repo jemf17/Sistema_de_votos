a923817b5c56a6aff24787c6ce89d8468b608740
jemf <jemf6montenegro@gmail.com>
2018-08-21 05:40:20 -0300

    hice la coneccion entre sql con java falta la url

--- Proyecto_adm/src/proyecto_adm/Conector.java
+++ Proyecto_adm/src/proyecto_adm/Conector.java
@@ -5,10 +5,23 @@
  */
 package proyecto_adm;
 
-/**
- *
- * @author jemf
- */
+import java.sql.*;
+
 public class Conector {
+    private PreparedStatement psInsertar;
+    private Statement statem;
+    private Connection con;
     
+    public void insertar(Publicaciones p) throws SQLException{
+        String url = "";
+        con = DriverManager.getConnection(url);
+        statem = con.createStatement();
+        
+        psInsertar = con.prepareStatement("");
+        
+        psInsertar.setFloat(1, p.getPorcentaje());
+        psInsertar.setString(2, p.getPublicacion());
+        
+        psInsertar.executeUpdate();
+    }
 }
